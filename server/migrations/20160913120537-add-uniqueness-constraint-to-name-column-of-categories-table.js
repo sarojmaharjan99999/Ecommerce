08be0dfcb0af2316b4098df7181c6fc6b4289277
Shree Ram Neupane <shreeramneupane0@gmail.com>
2016-09-15 11:38:29 +0545

    EC-1 makes the Categories table name field case insensitive

--- server/migrations/20160913120537-add-uniqueness-constraint-to-name-column-of-categories-table.js
+++ server/migrations/20160913120537-add-uniqueness-constraint-to-name-column-of-categories-table.js
@@ -3,43 +3,18 @@
 module.exports = {
   up: function (queryInterface, Sequelize) {
     return [
-      queryInterface.changeColumn(
+      queryInterface.addIndex(
         'Categories',
-        'name',
+        [Sequelize.fn('lower', Sequelize.col('name'))],
         {
-          type: Sequelize.STRING,
-          allowNull: false,
-          unique: true
-        },
-        {
-          indexes: [
-            { name: 'unique_name', unique: true, fields: [sequelize.fn('lower', sequelize.col('name'))] }
-          ]
+          indexName: 'uniqueName',
+    	  indicesType: 'UNIQUE'
         }
       )
     ];
   },
 
-  // TODO Not dropping constraint
   down: function (queryInterface, Sequelize) {
-    return queryInterface.changeColumn(
-        'Categories',
-        'name',
-        {
-          type: Sequelize.STRING,
-          allowNull: false,
-          unique: false
-        }
-      );
+    queryInterface.removeIndex('Categories', 'uniqueName')
   }
 };
-
-var Model = sequelize.define('Company', {
-  name: {
-    type: DataTypes.STRING
-  }
-}, {
-  indexes: [
-    { name: 'unique_name', unique: true, fields: [sequelize.fn('lower', sequelize.col('name'))] }
-  ]
-});
